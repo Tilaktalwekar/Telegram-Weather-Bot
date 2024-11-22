c942073ad8907428a189ca7f41b5ef00d37cbab4
Tilak Talwekar <110180919+Tilaktalwekar@users.noreply.github.com>
2024-11-22 23:45:20 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -101,20 +101,6 @@ const sendWeatherUpdates = async () => {
     }
   }
 
-  const express = require("express");
-const app = express();
-
-const PORT =  3000;
-
-// A simple route to make sure Render knows it's running
-app.get("/", (req, res) => {
-  res.send("Bot is up and running!");
-});
-
-// Start the Express server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 };
 
