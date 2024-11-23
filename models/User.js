0e52a976273612aa4a5d1e00e0d83c1e75c5fb06
Tilak Talwekar <110180919+Tilaktalwekar@users.noreply.github.com>
2024-11-23 19:34:36 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
   chatId: { type: String, required: true, unique: true },
   city: { type: String },
   subscribed: { type: Boolean, default: true },
+  blocked: { type: Boolean, default: false },
+
 });
 
 module.exports = mongoose.model("User", userSchema);
